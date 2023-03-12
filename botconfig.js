e9a59921eff7bb4eae11718e076289d2bb074868
unknown <kondratiuk.taras123@gmail.com>
2023-03-13 00:45:09 +0200

    add more vars

--- botconfig.js
+++ botconfig.js
@@ -5,8 +5,8 @@ module.exports = {
   Port: 3000, //Which port website gonna be hosted
   SupportServer: "https://discord.gg/sbySMS7m3v", // Support Server Link
   Token: process.env.DISCORD_TOKEN || "", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", // Discord Client ID
-  ClientSecret: process.env.Discord_ClientSecret || "", // Discord Client Secret
+  ClientID: process.env.DISCORD_CLIENT_ID || "", // Discord Client ID
+  ClientSecret: process.env.DISCORD_CLIENT_SECRET || "", // Discord Client Secret
   Scopes: ["identify", "guilds", "applications.commands"], // Discord OAuth2 Scopes
   ServerDeafen: true, // If you want bot to stay deafened
   DefaultVolume: 100, // Sets the default volume of the bot, You can change this number anywhere from 1 to 9007199254740991 (JS Integer limit. If you do set it to that, you're a monster.)
